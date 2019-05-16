823d3698cef2fae907920107949d627d96d9c023
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2019-05-16 09:43:47 +0200

    add X-XSS-Protection

---
+++ src/XssProtection/XssProtection.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * @link https://github.com/ixocreate
+ * @copyright IXOCREATE GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Ixocreate\SecurityHeader\XssProtection;
+
+use Psr\Http\Message\ResponseInterface;
+
+final class XssProtection
+{
+    private const MODE_DISABLE = '0';
+    private const MODE_ENABLE = '1';
+    private const MODE_BLOCK = '1; mode=block';
+    private const MODE_REPORT = '1; report=%s';
+
+    /**
+     * @var string
+     */
+    private $mode = self::MODE_BLOCK;
+
+    /**
+     * @return XssProtection
+     */
+    public function disable(): XssProtection
+    {
+        $xssProtection = clone $this;
+        $xssProtection->mode = self::MODE_DISABLE;
+
+        return $xssProtection;
+    }
+
+    /**
+     * @return XssProtection
+     */
+    public function enable(): XssProtection
+    {
+        $xssProtection = clone $this;
+        $xssProtection->mode = self::MODE_ENABLE;
+
+        return $xssProtection;
+    }
+
+    /**
+     * @return XssProtection
+     */
+    public function block(): XssProtection
+    {
+        $xssProtection = clone $this;
+        $xssProtection->mode = self::MODE_BLOCK;
+
+        return $xssProtection;
+    }
+
+    /**
+     * @param string $url
+     * @return XssProtection
+     */
+    public function report(string $url): XssProtection
+    {
+        $xssProtection = clone $this;
+        $xssProtection->mode = self::MODE_REPORT;
+        $xssProtection->mode = \sprintf($xssProtection->mode, $url);
+
+        return $xssProtection;
+    }
+
+    /**
+     *
+     */
+    public function send(): void
+    {
+        \header('X-XSS-Protection: ' . $this->mode);
+    }
+
+    /**
+     * @param ResponseInterface $response
+     * @return ResponseInterface
+     */
+    public function response(ResponseInterface $response): ResponseInterface
+    {
+        return $response->withHeader('x-xss-protection', $this->mode);
+    }
+}
---
+++ tests/src/XssProtection/XssProtectionTest.php
@@ -0,0 +1,185 @@
+<?php
+/**
+ * @link https://github.com/ixocreate
+ * @copyright IXOCREATE GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Ixocreate\Test\SecurityHeader\XssProtection;
+
+use Ixocreate\SecurityHeader\FrameOptions\FrameOptions;
+use Ixocreate\SecurityHeader\XssProtection\XssProtection;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * @covers \Ixocreate\SecurityHeader\XssProtection\XssProtection
+ */
+class XssProtectionTest extends TestCase
+{
+    /**
+     * @var MockObject
+     */
+    private $responseMock;
+
+    public function setUp()
+    {
+        $this->responseMock = $this->createMock(ResponseInterface::class);
+        $this->responseMock->method('withHeader')->willReturnSelf();
+    }
+
+    private function reflectionPrivateProperty(string $propertyName, XssProtection $xssProtection)
+    {
+        $reflection = new \ReflectionClass($xssProtection);
+        $property = $reflection->getProperty($propertyName);
+        $property->setAccessible(true);
+
+        return $property->getValue($xssProtection);
+    }
+
+    public function testDefaults()
+    {
+        $xssProtection = new XssProtection();
+        $this->assertSame('1; mode=block', $this->reflectionPrivateProperty('mode', $xssProtection));
+    }
+
+    public function testDisableImmutable()
+    {
+        $xssProtection = new XssProtection();
+        $newXssProtection = $xssProtection->disable();
+        $this->assertNotSame($newXssProtection, $xssProtection);
+    }
+
+    public function testEnableImmutable()
+    {
+        $xssProtection = new XssProtection();
+        $newXssProtection = $xssProtection->enable();
+        $this->assertNotSame($newXssProtection, $xssProtection);
+    }
+
+    public function testBlockImmutable()
+    {
+        $xssProtection = new XssProtection();
+        $newXssProtection = $xssProtection->block();
+        $this->assertNotSame($newXssProtection, $xssProtection);
+    }
+
+    public function testReportImmutable()
+    {
+        $xssProtection = new XssProtection();
+        $newXssProtection = $xssProtection->report('https://www.ixocreate.com');
+        $this->assertNotSame($newXssProtection, $xssProtection);
+    }
+
+
+    public function testResponseDisable()
+    {
+        $this->responseMock
+            ->expects($this->once())
+            ->method('withHeader')
+            ->with('x-xss-protection', '0');
+        $xssProtection = new XssProtection();
+        $xssProtection = $xssProtection->disable();
+
+        $xssProtection->response($this->responseMock);
+    }
+
+    public function testResponseEnable()
+    {
+        $this->responseMock
+            ->expects($this->once())
+            ->method('withHeader')
+            ->with('x-xss-protection', '1');
+        $xssProtection = new XssProtection();
+        $xssProtection = $xssProtection->enable();
+
+        $xssProtection->response($this->responseMock);
+    }
+
+    public function testResponseBlock()
+    {
+        $this->responseMock
+            ->expects($this->once())
+            ->method('withHeader')
+            ->with('x-xss-protection', '1; mode=block');
+        $xssProtection = new XssProtection();
+        $xssProtection = $xssProtection->block();
+
+        $xssProtection->response($this->responseMock);
+    }
+
+    public function testResponseReport()
+    {
+        $this->responseMock
+            ->expects($this->once())
+            ->method('withHeader')
+            ->with('x-xss-protection', '1; report=https://www.ixocreate.com');
+        $xssProtection = new XssProtection();
+        $xssProtection = $xssProtection->report('https://www.ixocreate.com');
+
+        $xssProtection->response($this->responseMock);
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testSendDisable()
+    {
+        $xssProtection = new XssProtection();
+        $xssProtection = $xssProtection->disable();
+
+        $xssProtection->send();
+        $this->assertContains(
+            'X-XSS-Protection: 0',
+            xdebug_get_headers()
+        );
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testSendEnable()
+    {
+        $xssProtection = new XssProtection();
+        $xssProtection = $xssProtection->enable();
+
+        $xssProtection->send();
+        $this->assertContains(
+            'X-XSS-Protection: 1',
+            xdebug_get_headers()
+        );
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testSendBlock()
+    {
+        $xssProtection = new XssProtection();
+        $xssProtection = $xssProtection->block();
+
+        $xssProtection->send();
+        $this->assertContains(
+            'X-XSS-Protection: 1; mode=block',
+            xdebug_get_headers()
+        );
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testSendReport()
+    {
+        $xssProtection = new XssProtection();
+        $xssProtection = $xssProtection->report('https://www.ixocreate.com');
+
+        $xssProtection->send();
+        $this->assertContains(
+            'X-XSS-Protection: 1; report=https://www.ixocreate.com',
+            xdebug_get_headers()
+        );
+    }
+}

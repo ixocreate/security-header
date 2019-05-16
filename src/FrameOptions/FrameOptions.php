452d53a48c6e07e7a4128eddf90b352589845653
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2019-05-16 09:17:26 +0200

    added support for X-Frame-Options header

---
+++ src/FrameOptions/FrameOptions.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace Ixocreate\SecurityHeader\FrameOptions;
+
+use Psr\Http\Message\ResponseInterface;
+
+final class FrameOptions
+{
+    private const OPTION_DENY = 'deny';
+    private const OPTION_SAMEORIGIN = 'sameorigin';
+
+    /**
+     * @var string
+     */
+    private $option = self::OPTION_DENY;
+
+    /**
+     * @return FrameOptions
+     */
+    public function deny(): FrameOptions
+    {
+        $frameOption = clone $this;
+        $frameOption->option = self::OPTION_DENY;
+
+        return $frameOption;
+    }
+
+    /**
+     * @return FrameOptions
+     */
+    public function sameOrigin(): FrameOptions
+    {
+        $frameOption = clone $this;
+        $frameOption->option = self::OPTION_SAMEORIGIN;
+
+        return $frameOption;
+    }
+
+    /**
+     *
+     */
+    public function send(): void
+    {
+        \header('X-Frame-Options: '. $this->option);
+    }
+
+    /**
+     * @param ResponseInterface $response
+     * @return ResponseInterface
+     */
+    public function response(ResponseInterface $response): ResponseInterface
+    {
+        return $response->withHeader('x-frame-options', $this->option);
+    }
+}
---
+++ tests/src/FrameOptions/FrameOptionsTest.php
@@ -0,0 +1,109 @@
+<?php
+declare(strict_types=1);
+
+namespace Ixocreate\Test\SecurityHeader\FrameOptions;
+
+use Ixocreate\SecurityHeader\FrameOptions\FrameOptions;
+use Ixocreate\SecurityHeader\Hsts\Hsts;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * @covers \Ixocreate\SecurityHeader\FrameOptions\FrameOptions
+ */
+class FrameOptionsTest extends TestCase
+{
+    /**
+     * @var MockObject
+     */
+    private $responseMock;
+
+    public function setUp()
+    {
+        $this->responseMock = $this->createMock(ResponseInterface::class);
+        $this->responseMock->method('withHeader')->willReturnSelf();
+    }
+
+    private function reflectionPrivateProperty(string $propertyName, FrameOptions $frameOptions)
+    {
+        $reflection = new \ReflectionClass($frameOptions);
+        $property = $reflection->getProperty($propertyName);
+        $property->setAccessible(true);
+
+        return $property->getValue($frameOptions);
+    }
+
+    public function testDefaults()
+    {
+        $frameOptions = new FrameOptions();
+        $this->assertSame('deny', $this->reflectionPrivateProperty('option', $frameOptions));
+    }
+
+    public function testDenyImmutable()
+    {
+        $frameOptions = new FrameOptions();
+        $newFrameOptions = $frameOptions->deny();
+        $this->assertNotSame($newFrameOptions, $frameOptions);
+    }
+
+    public function testSameOriginImmutable()
+    {
+        $frameOptions = new FrameOptions();
+        $newFrameOptions = $frameOptions->sameOrigin();
+        $this->assertNotSame($newFrameOptions, $frameOptions);
+    }
+
+
+    public function testResponseDeny()
+    {
+        $this->responseMock
+            ->expects($this->once())
+            ->method('withHeader')
+            ->with('x-frame-options', 'deny');
+        $frameOptions = new FrameOptions();
+        $frameOptions = $frameOptions->deny();
+
+        $frameOptions->response($this->responseMock);
+    }
+
+    public function testResponseSameOrigin()
+    {
+        $this->responseMock
+            ->expects($this->once())
+            ->method('withHeader')
+            ->with('x-frame-options', 'sameorigin');
+        $frameOptions = new FrameOptions();
+        $frameOptions = $frameOptions->sameOrigin();
+
+        $frameOptions->response($this->responseMock);
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testSendDeny()
+    {
+        $frameOptions = new FrameOptions();
+        $frameOptions = $frameOptions->deny();
+
+        $frameOptions->send();
+        $this->assertContains(
+            'X-Frame-Options: deny', xdebug_get_headers()
+        );
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testSendSameOrigin()
+    {
+        $frameOptions = new FrameOptions();
+        $frameOptions = $frameOptions->sameOrigin();
+
+        $frameOptions->send();
+        $this->assertContains(
+            'X-Frame-Options: sameorigin', xdebug_get_headers()
+        );
+    }
+}
